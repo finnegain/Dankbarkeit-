28663071a14b35d01344055e861343abcc28281e
finnpfeilschifter <finn.pfeilschifter@tha.de>
2026-06-07 12:47:21 +0200

    Refactor

--- PyWebanwendung/PyWebanwendung/urls.py
+++ PyWebanwendung/PyWebanwendung/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for awesomegallery project.
+URL configuration for PyWebanwendung project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/

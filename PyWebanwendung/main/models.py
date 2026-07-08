15414aa284fee820f87efbe95210ac3113c02f06
finnpfeilschifter <finn.pfeilschifter@tha.de>
2026-07-08 07:34:49 +0200

    Refactor Versuch 2

--- PyWebanwendung/main/models.py
+++ PyWebanwendung/main/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
-    title = models.CharField(max_length=200)
-
 class TextMessage(models.Model):
     text = models.TextField(max_length=500)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='text_messages')

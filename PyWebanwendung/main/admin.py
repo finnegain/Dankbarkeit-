4977da1f7df8abbdf6109306566fdc66d2df590f
finnpfeilschifter <finn.pfeilschifter@tha.de>
2026-07-08 07:43:10 +0200

    Refactor Versuch 2

--- PyWebanwendung/main/admin.py
+++ PyWebanwendung/main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Image
+from .models import TextMessage
 
-admin.site.register(Image)
+admin.site.register(TextMessage)
